b12dd2346a05c874cbe0b4b68a7772854ad3adab
evan <guarneri1682@gmail.com>
2024-02-29 17:15:23 -0800

    240229_library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import Pizza from './pizza';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Movie from "./movie";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div>
-      <h1>hihi</h1>
-      <Pizza />
-      
+      {" "}
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/pizza.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Pizza(){
-    return <h3>I love pizza</h3>
-}
-
-export default Pizza;
